0180ee4787d50859bd63f7567003af90695e2bef
jaybrai <144754151+jaybrai@users.noreply.github.com>
2024-10-28 23:01:44 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.16.40.166:9000/',
+  serverUrl: 'http://18.118.17.240:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
