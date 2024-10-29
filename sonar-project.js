9034f5445f38fd93b79eab62d4a159d5ce9301cc
jaybrai <144754151+jaybrai@users.noreply.github.com>
2024-10-29 00:02:32 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
 	    'sonar.login': 'b76d58667fb44accbeec95a2eb1cc8cc984ef2ec',
-	    'sonar.password': 'admin',
+	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
